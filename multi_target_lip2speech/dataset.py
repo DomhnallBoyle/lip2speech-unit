Auto-AVSR transforms fix, 6th layer for speech units and use captions for AVSpeech downloader

multi_target_lip2speech/dataset.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from typing import Any, List, Optional, Union
 
 import numpy as np
-
 import torch
 import torch.nn.functional as F
 from python_speech_features import logfbank
@@ -66,13 +65,15 @@ class MultiTargetDataset(AVHubertDataset):
             noise_num=1,
             time_mask: bool = False,
             random_erase: bool = False,
-            text_supervision: bool = False
+            text_supervision: bool = False,
+            auto_avsr: bool = False
     ):
         # load gt if available
         self.gt_path = gt_path
         self.text_supervision = text_supervision and Path(gt_path).exists()
         self.sentence_processor = SentenceProcessor() if self.text_supervision else None
         self.gt_d = self.load_gt_data(gt_path=gt_path) if self.text_supervision else None
+        self.auto_avsr = auto_avsr
 
         self.label_rates = (
             [label_rates for _ in range(len(label_paths))]
@@ -127,18 +128,41 @@ class MultiTargetDataset(AVHubertDataset):
         self.pad_audio = pad_audio
         self.normalize = normalize
         if image_aug:
-            self.transform = custom_utils.Compose([
-                custom_utils.Normalize( 0.0,255.0 ),
-                custom_utils.RandomCrop((image_crop_size, image_crop_size)),
-                custom_utils.HorizontalFlip(0.5),
-                custom_utils.Normalize(image_mean, image_std) ]
-                + ([custom_utils.RandomErase(0.5)] if random_erase else [])
-                + ([custom_utils.TimeMask()] if time_mask else []) )
+            if self.auto_avsr:
+                transforms = [
+                    custom_utils.Normalize(0.0, 255.0),
+                    custom_utils.RandomCrop((image_crop_size, image_crop_size)),
+                    custom_utils.Grayscale(),
+                    custom_utils.HorizontalFlip(0.5),
+                    custom_utils.Normalize(image_mean, image_std) 
+                ]
+            else:
+                transforms = [
+                    custom_utils.Normalize(0.0, 255.0),
+                    custom_utils.RandomCrop((image_crop_size, image_crop_size)),
+                    custom_utils.HorizontalFlip(0.5),  # flip around y-axis
+                    custom_utils.Normalize(image_mean, image_std) 
+                ]
+
+            transforms += [custom_utils.RandomErase(0.5)] if random_erase else []
+            transforms += [custom_utils.TimeMask()] if time_mask else []
+            self.transform = custom_utils.Compose(transforms)
         else:
-            self.transform = custom_utils.Compose([
-                custom_utils.Normalize( 0.0,255.0 ),
-                custom_utils.CenterCrop((image_crop_size, image_crop_size)),
-                custom_utils.Normalize(image_mean, image_std) ])
+            if self.auto_avsr:
+                transforms = [
+                    custom_utils.Normalize(0.0, 255.0),
+                    custom_utils.CenterCrop((image_crop_size, image_crop_size)),
+                    custom_utils.Grayscale(),
+                    custom_utils.Normalize(image_mean, image_std)
+                ]
+            else:
+                transforms = [
+                    custom_utils.Normalize(0.0, 255.0),
+                    custom_utils.CenterCrop((image_crop_size, image_crop_size)),
+                    custom_utils.Normalize(image_mean, image_std)
+                ]
+            self.transform = custom_utils.Compose(transforms)
+
         logger.info(f"image transform: {self.transform}")
 
         logger.info(
@@ -185,6 +209,17 @@ class MultiTargetDataset(AVHubertDataset):
             raise FileNotFoundError(f"{spk_emb_fn} does not exist")
 
         return mel, spk_emb
+
+    def load_video(self, audio_name):
+        if not self.auto_avsr:
+            return super().load_video(audio_name)
+
+        # read video in colour (BGR), convert to grayscale later
+        feats = custom_utils.load_video(os.path.join(self.audio_root, audio_name), grayscale=False)
+        feats = self.transform(feats)
+        feats = np.expand_dims(feats, axis=-1)
+        
+        return feats
 
     def __getitem__(self, index):
         sample = super().__getitem__(index)  # speech units loaded in base class

multi_target_lip2speech/sequence_generator.py
@@ -120,7 +120,7 @@ class MultiTargetSequenceGenerator(SequenceGenerator):
             mels = [mel[:target_length*2] for mel, target_length in zip(mels, sample['target_lengths'])]
             sample['mels'] = mels
 
-        if encoder_outs[0]['encoder_char'] is not None:
+        if 'encoder_char' in encoder_outs[0]:
             encoder_char = encoder_outs[0]['encoder_char'].cpu()  # output = T, 1, C (1 sample in batch)
             encoder_char_softmax = torch.nn.functional.softmax(encoder_char, dim=2)[:, 0, :]  # output = T, C
             

multi_target_lip2speech/utils_aug.py
@@ -3,14 +3,17 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+import math
 
 import cv2
 import torch
 import random
 import numpy as np
+import torchvision
 from typing import Dict, List, Optional, Tuple
 
-def load_video(path):
+
+def load_video(path, grayscale=True):
     for i in range(3):
         try:
             cap = cv2.VideoCapture(path)
@@ -18,7 +21,8 @@ def load_video(path):
             while True:
                 ret, frame = cap.read()
                 if ret:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    if grayscale:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                     frames.append(frame)
                 else:
                     break
@@ -53,6 +57,18 @@ class Compose(object):
         return format_string
 
 
+class Grayscale:
+
+    def __init__(self):
+        self.transform = torchvision.transforms.Grayscale()
+
+    def __call__(self, frames):
+        frames = torch.from_numpy(frames).permute(0, 3, 1, 2)  # transform requires [T, C, H, W]
+        frames = self.transform(frames)  # output = [T, 1, H, W]
+
+        return frames.squeeze(1).numpy()  # remove single dim
+
+
 class Normalize(object):
     """Normalize a ndarray image with mean and standard deviation.
     """
@@ -74,6 +90,7 @@ class Normalize(object):
     def __repr__(self):
         return self.__class__.__name__+'(mean={0}, std={1})'.format(self.mean, self.std)
 
+
 class CenterCrop(object):
     """Crop the given image at the center
     """
@@ -87,7 +104,7 @@ class CenterCrop(object):
         Returns:
             numpy.ndarray: Cropped image.
         """
-        t, h, w = frames.shape
+        t, h, w = frames.shape[:3]
         th, tw = self.size
         delta_w = int(round((w - tw))/2.)
         delta_h = int(round((h - th))/2.)
@@ -109,7 +126,7 @@ class RandomCrop(object):
         Returns:
             numpy.ndarray: Cropped image.
         """
-        t, h, w = frames.shape
+        t, h, w = frames.shape[:3]
         th, tw = self.size
         delta_w = random.randint(0, w-tw)
         delta_h = random.randint(0, h-th)
@@ -118,6 +135,7 @@ class RandomCrop(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(size={0})'.format(self.size)
+
 
 class HorizontalFlip(object):
     """Flip image horizontally.
@@ -139,7 +157,7 @@ class HorizontalFlip(object):
                 frames[index] = cv2.flip(frames[index], 1)
         return frames
 
-import math
+
 class RandomErase(object):
     def __init__(self, p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), replace_with_zero=True):
         self.p = p
@@ -173,6 +191,7 @@ class RandomErase(object):
 
         return frames
 
+
 class TimeMask(object):
     """time mask
     """
@@ -200,6 +219,7 @@ class TimeMask(object):
                 cloned[i*self.hop_frame+mask_start : i*self.hop_frame+mask_start+mask_len] = cloned.mean()
 
         return cloned
+
 
 def compute_mask_indices(
     shape: Tuple[int, int],
@@ -330,6 +350,7 @@ def compute_mask_indices(
         ends.append(start_indices+lengths)
         batch_indexes.append(np.zeros([len(start_indices)])+i)
     return mask, np.concatenate(starts).astype(np.int64), np.concatenate(ends).astype(np.int64), np.concatenate(batch_indexes).astype(np.int64)
+
 
 def find_runs(x):
     """Find runs of consecutive items in an array."""

server.py
@@ -80,11 +80,11 @@ def setup(args, args_path):
         with open(args_path, 'r') as f:
             args = SimpleNamespace(**json.load(f))
 
-    for k in ['default_audios', 'default_audio', 'web_client_run_asr', 'web_client_streaming', 'prod']:
+    for k in ['default_audios', 'default_audio_id', 'web_client_run_asr', 'web_client_streaming', 'prod']:
         assert k in args.__dict__, f'"{k}" not in args'
 
     assert len(args.default_audios) > 0, 'Default audios required'
-    assert args.default_audio in args.default_audios, f'Default audio "{args.default_audio}" does not exist in mapping'
+    assert any([args.default_audio_id == d['id'] for d in args.default_audios]), f'Default audio "{args.default_audio_id}" does not exist in mapping'
 
     # setup static and server directories
     for d in [config.INPUTS_PATH, config.STATIC_PATH]:
@@ -99,26 +99,23 @@ def create_app(args=None, args_path=None):
     args = setup(args, args_path)
 
     # move any audio files to static and pre-load speaker embeddings for them
-    default_audio_id = None
+    default_audio_id = args.default_audio_id
     speaker_embedding_lookup = {}
     default_audios_list = []
-    for audio_name, audio_path in args.default_audios.items():
+    for audio_d in args.default_audios:
+        audio_id, audio_name, audio_path = audio_d['id'], audio_d['name'], audio_d['path']
+
         audio_path = Path(audio_path)
         assert audio_path.exists(), f'{audio_path} does not exist'
 
-        audio_id = str(uuid.uuid4())
         static_audio_path = config.STATIC_PATH.joinpath(f'{audio_id}.wav')
-        shutil.copyfile(audio_path, static_audio_path)
+        shutil.copyfile(audio_path, static_audio_path)  # will replace if destination exists
 
         try:
             speaker_embedding_lookup[audio_id] = _get_speaker_embedding(audio_path=str(static_audio_path))
         except requests.exceptions.ConnectionError:
             logging.warning('Speaker embedding server is down...')
         
-        # set the default audio id if no audio input supplied
-        if args.default_audio == audio_name:
-            default_audio_id = audio_id
-
         default_audios_list.append({
             'id': audio_id,
             'name': audio_name
@@ -210,7 +207,9 @@ def create_app(args=None, args_path=None):
                 return {'message': 'Speaker embedding server not available'}, HTTPStatus.INTERNAL_SERVER_ERROR
         else:
             logging.info(f'Using speaker embedding from lookup - {audio_id}...')
-            speaker_embedding = speaker_embedding_lookup[audio_id]  # use default or supplied audio id
+            speaker_embedding = speaker_embedding_lookup.get(audio_id)  # use default or supplied audio id
+            if speaker_embedding is None:
+                return {'message': f'Default audio id {audio_id} does not exist'}, HTTPStatus.BAD_REQUEST
         np.save(config.SPK_EMB_DIRECTORY.joinpath(f'{uid}.npy'), speaker_embedding)
 
         # create file.list for extracting mouth frames